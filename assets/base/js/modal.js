74616ebb80d39f879cdf86c29cc5a4f1d36e4fc1
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-06-24 19:34:26 +0430

    added worklow of charg modals

--- assets/base/js/modal.js
+++ assets/base/js/modal.js
@@ -4,12 +4,31 @@ const firstSectionLeftVideos = document.querySelectorAll('.first-section-left-vi
 const videoModal = document.getElementById('video-modal');
 const chargeAccountBtn = document.querySelectorAll('.charge-account-btn');
 const chargeModal = document.getElementById('charge-modal');
+const chargeModalForm = document.getElementById('charge-modal-form');
+const chargeModalStep2 = document.getElementById('charge-modal-step2');
+const modalGoBackBtn = document.getElementById('modal-go-back-btn');
+const chargeModalStep2SubmitBtn = document.getElementById('charge-modal-step2-submit-btn');
+const chargeModalSuccessful = document.getElementById('charge-modal-successful');
+const chargedSuccsesfulyAcceptBtn = document.getElementById('charge-successful-accept-btn');
+const denibtn = document.getElementById('deni-btn');
 
 // A Function That Takes Modal As An Argument And Removes It And Removes Dark Bg Of It.
 function closeModal(modal) {
     modal.classList.add('d-none');
     modalBg.classList.add('pointer-event-none');
     modalBg.classList.add('opacity-0');
+}
+
+// A Function That Shows Next Modal And Closes Current Open Modal.
+function showNextModal(currentModal, nextModal) {
+    closeModal(currentModal);
+    showModal(nextModal);
+}
+
+// A Function That Shows Previous Modal And Closes Current Open Modal.
+function showPreviousModal(currentModal, prevModal) {
+    closeModal(currentModal);
+    showModal(prevModal);
 }
 
 // A Function That Shows Given Modal
@@ -30,10 +49,21 @@ function showModal(modal) {
 firstSectionLeftVideos.forEach(video => video.addEventListener('click', () => showModal(videoModal)))
 chargeAccountBtn.forEach(btn => btn.addEventListener('click', () => showModal(chargeModal)));
 
-document.querySelectorAll('.deni-btn').forEach(btn => btn.addEventListener('click', () => closeModal(btn.parentElement.parentElement)));
+// Adding Event Listener On Go Back Button That Shows Previous Modal
+modalGoBackBtn.addEventListener('click', () => showPreviousModal(chargeModalStep2, chargeModal))
 
-document.querySelectorAll('.modal-go-back-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        // if ()
-    })
+// Adding Event Listener On Second Step Of Charge Modal That Shows Next Modal
+chargeModalStep2SubmitBtn.addEventListener('click', () => showNextModal(chargeModalStep2, chargeModalSuccessful))
+
+// Adding Event Listener Charge Was Successful Modal That Closes Modal;
+chargedSuccsesfulyAcceptBtn.addEventListener('click', () => closeModal(chargeModalSuccessful));
+
+// Adding Event Listner On Deni Button That Again Closes Modal
+denibtn.addEventListener('click', () => closeModal(chargeModalStep2));
+
+// Adding Event Listener On Form Element Of Charge Modal That Listens To Submit And Prevents From Default Action
+// And Shows Its Next Modal.
+chargeModalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    showNextModal(chargeModal, chargeModalStep2);
 })
